804064b04b21d242b069f893b2a95d24abd4939e
AndereX-dev <andreas.osdal@hotmail.com>
2024-08-13 18:03:31 +0200

    added stuff

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+// If or Else statment
 
+const hour = 10
+
+//Checking if the clock is less than 12
+if (hour < 12) {
+    console.log("Good morning")
+} else if (hour === 12) {
+    //Checking if the clock is 12 
+    console.log("The clock is 12!")
+} else if (hour > 14 && hour < 18) {
+    //Checking if the clock is between 14 and 18
+    console.log("The clock is between 14 and 18")
+} else {
+    //Checking out if the time is past 12
+    console.log("Good day")
+}
+//
+//
+
+//Simple calculator
+const calculator = document.querySelector("#calculator").value;
+console.log(calculator)
+
+calculator.addEventListener("click", function (){
+    const calculatorValue = document.querySelector("#calculator").value;
+    console.log(calculatorValue)
+}) 
+
+const num1 = 10
+const num2 = 50
+let sum;
+
+if (num1 + num2) {
+    sum = num1 + num2
+    console.log(sum)
+} else if (num1 - num2) {
+    sum = num1 - num2
+    console.log(sum)
+} else if (num1 * num2) {
+    sum = num1 * num2
+    console.log(sum)
+} else if (num1 / num2) {
+    sum = num1 / num2
+    console.log(sum)
+} else {
+    sum = "Error"
+    console.log(sum)
+}
